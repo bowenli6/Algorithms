db4a276366e2495f339eda7efb5caff6a20876f9
Bowen Li <243742619@qq.com>
2022-01-08 23:36:07 +0800

    update Percolation.java

--- src/DataStructures/fundamentals/unionfind/Percolation.java
+++ src/DataStructures/fundamentals/unionfind/Percolation.java
@@ -28,55 +28,106 @@ public class Percolation {
      * The task is to write a computer program to estimate p*.
      */
 
-    private boolean[][] grid;           // the n-by-n grid of sites
-    private WeightedQuickUnionUF uf;    // the union find data structure
-    private int virtualTop;             // a virtual top site
-    private int virtualBottom;          // a virtual bottom site
-    private int numberOfOpenSites;      // the number of the open sites
-    private final int N;                // the size of the grid
+    private final int N;                        // the size of the grid
+    private int numberOfOpenSites;              // the number of the open sites
+    private final boolean[][] sites;            // the n-by-n grid of sites
+    private final int virtualTopSite;           // a virtual top site
+    private final int virtualBottomSite;        // a virtual bottom site
+    private final WeightedQuickUnionUF uf;      // the union find data structure
+
 
     // creates n-by-n grid, with all sites initially blocked
     public Percolation(int n) {
         if (n <= 0) throw new IllegalArgumentException("n cannot be smaller than 1");
         N = n;
-        grid = new boolean[n][n];
+        sites = new boolean[n][n];
+        virtualTopSite = n * n;
+        virtualBottomSite = n * n + 1;
         uf = new WeightedQuickUnionUF(n * n + 2);
         numberOfOpenSites = 0;
     }
 
-//    private int to1D(int row, int col)
+    private int index(int row, int col) {
+        return (row - 1) * N + (col - 1);
+    }
 
 
     // opens the site (row, col) if it is not open already
     public void open(int row, int col) {
         validate(row, col);
+        if (!sites[row-1][col-1]) {
+            sites[row-1][col-1] = true;
+            numberOfOpenSites++;
+            if (row == 1) uf.union(virtualTopSite, index(row, col));    //top row
+            if (row == N) uf.union(virtualBottomSite, index(row, col)); // bottom row
+            connectNeighbors(row, col);
+        }
+    }
+
+    // union possible neighbors
+    private void connectNeighbors(int row, int col) {
+        int it = index(row, col);
+        if (row > 1 && isOpen(row - 1, col)) {
+            // union with its upper site
+            uf.union(it, index(row - 1, col));
+        }
+        if (row < N && isOpen(row + 1, col)) {
+            // union with its lower site
+            uf.union(it, index(row + 1, col));
+        }
+        if (col > 1 && isOpen(row, col - 1)) {
+            // union with its left site
+            uf.union(it, index(row, col - 1));
+        }
+        if (col < N && isOpen(row, col + 1)) {
+            // union with its right site
+            uf.union(it, index(row, col + 1));
+        }
     }
 
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
         validate(row, col);
-        return grid[row-1][col-1];
+        return sites[row-1][col-1];
     }
 
-//    // is the site (row, col) full?
-//    public boolean isFull(int row, int col)
-//
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col) {
+        validate(row, col);
+        int it = uf.find(index(row, col));
+        return isOpen(row, col) && it == uf.find(virtualTopSite);
+    }
+
     // returns the number of open sites
     public int numberOfOpenSites() {
         return numberOfOpenSites;
     }
-//
-//    // does the system percolate?
-//    public boolean percolates()
+
+    // does the system percolate?
+    public boolean percolates() {
+        return uf.find(virtualTopSite) == uf.find(virtualBottomSite);
+    }
 
     // validate that row is col valid index
     private void validate(int row, int col) {
-        if (row < 0 || row > N || col < 0  || col > N) {
-            throw new IllegalArgumentException("The index is outside its prescribed range");
+        if (row < 1 || row > N || col < 1  || col > N) {
+            throw new IllegalArgumentException("The index is outside of its prescribed range");
         }
     }
     // test client (optional)
     public static void main(String[] args) {
+        Percolation p = new Percolation(3);
+        p.open(2, 1);
+        p.open(1, 1);
+        p.open(1, 3);
+        p.open(2,2);
+        System.out.println(p.isFull(1, 1));
+        System.out.println(p.isFull(1, 2));
+        System.out.println(p.isFull(1, 3));
+        System.out.println(p.isOpen(2, 2));
+        System.out.println(p.isFull(2, 2));
+        p.open(3, 1);
+        System.out.println(p.percolates());
     }
 
 }
